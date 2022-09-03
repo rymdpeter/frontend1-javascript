314a52d54507f6af4c680fdeaa910e3178a3cdd3
rymdpeter <pka.khl@gmail.com>
2022-09-03 15:53:52 +0200

    struktur

---
+++ 01-variabler/exempel.js
@@ -0,0 +1,8 @@
+const name = "Peter"
+let age = 39
+
+console.log("Hello world, my name is " + name)
+// name = "Johan";
+// console.log("Hello world, now my name is " + name)
+
+console.log(`I'm ${age += 1} years old now`)
