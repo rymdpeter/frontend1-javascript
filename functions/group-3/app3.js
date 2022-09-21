4c0e9d30eb84d2df353335710f559a7ce4c09106
AlexSaxena <saxenaalex@gmail.com>
2022-09-21 11:22:42 +0200

    Adds codebase to repo

---
+++ functions/group-3/app3.js
@@ -0,0 +1,7 @@
+console.log("program 3 här");
+
+let kelvin = 305
+let celsius = kelvin - 273
+let fahrenheit = celsius * (9 / 5) + 32
+
+console.log(`${kelvin} degrees Kelvin is ${celsius} degrees Celcius and ${fahrenheit} degrees Fahrenheit.`)
