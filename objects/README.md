233a66070e71f2af185f5d12aa3290da9b1042cc
rymdpeter <pka.khl@gmail.com>
2022-09-21 15:07:22 +0200

    app.js modified, instructions pending

---
+++ objects/README.md
@@ -0,0 +1,11 @@
+# Instruction
+
+## 1. create branch
+One group member creates a new branch locally from exercise-1
+```
+    git checkout -b excericise-1/Group-x
+```
+
+## 2. Create folder under path "/objects/group-x"
+
+## 3. 
--- objects/index.js
+++ objects/index.js
@@ -1,72 +1,65 @@
 
-// const students = require("./groups")
+ // const students = require("./groups")
 const students = require("../data/groups")
 
 const allStudents = students.all // read students from imported module
-let ambitionDefined
-let groupsAssigned
+let ambitionDefined,
+    groupsAssigned
+
 
 function assignGroup(index, size) {
     let group = Math.floor((index + 1) / size) + 1
-    let currentStudent = allStudents[index]
-
-    currentStudent.group = group
-    // console.log(currentStudent.firstname + " assigned to group: " + group)
+    students.all[index].group = group
     index++
 
-    if(index < allStudents.length) {   
-        assignGroup(index, size)
-    } else {
-        groupsAssigned = true
-    }
+    if(index >= students.all.length) {
+        return true   
+    } 
+    assignGroup(index, size)
 }
 
 function applyAmbition(index) {
-    let currentStudent = allStudents[index]
-    currentStudent.hoursPerWeek = Math.floor(Math.random() * 24)
+    // TODO: Time isn't everything
+    // TODO: Scheduled time in the classroom should be accounted for?
+    students.all[index].hoursPerWeek = Math.floor(Math.random() * 24)
+    // students.all[index].hasJob = true 
+    // ... 
     index++
 
-    if (index < allStudents.length) {
-        applyAmbition(index)
-    } else {
-        ambitionDefined = true
+    if (index >= allStudents.length) {
+        return true // remember that it's good practice to have single point of return for a function
     }
+    applyAmbition(index)
 }
 
-function getStudent(index, email) {
-    
-    let currentStudent = allStudents[index]
-    
-    index++
-    if (currentStudent.email.toLowerCase() === email) {
-        selectedStudent = currentStudent
-        return
-    } else if (index < allStudents.length) {
-        getStudent(index, email)
-    } else {
-        return false
-    }
+const getStudent = (index) => {
+    let i = Number(index) 
+    if (isNaN(i) || students.all.length > i) i = 0 // TODO: fix this. What happens when we don't get good input?
+    else i = index
+    return students.all[i]
 }
-1
+const getStudents = () =>{
+    return students.all
+}
+
 const setupGroups = () => {
-    if(!groupsAssigned) {
-        assignGroup(0, 6)
-    }
-
-    if (!ambitionDefined) { 
-        // applyAmbition(0)
-    }
-
-    // console.debug(allStudents)
-
-    const args = process.argv.slice(2)
     
-    if(args.length > 0) {
-      selectedStudent = getStudent(0, args[0])
-      console.log(selectedStudent)
-    } 
-    
+    ambitionDefined = applyAmbition(0)
+    groupsAssigned = assignGroup(0, 6) // TODO: maybe this need more thought?
+
+    return students.all   
+}
+// TODO: return an array with only the students that belongs to a group with a specific index
+const getGroup = (arg) => {
+    return({
+        TODO: "return group " + arg
+        // students: students.all.splice(11,6) // TODO: this works as long as the array of students is sorted by group. Also the size 
+    })
 }
 
-setupGroups()
+exports._setupGroups = setupGroups
+exports._getStudents = getStudents
+exports._getStudent = getStudent
+exports._getGroup = getGroup
+exports._getStudent = getStudent
 
