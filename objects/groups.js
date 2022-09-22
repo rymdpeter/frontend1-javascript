7d8e214d6af0735af34674a576e93ce4d488fe9b
EmilSunden <emilsunjobb@gmail.com>
2022-09-22 11:02:54 +0200

    changed names in groups.js to that of our group

--- objects/groups.js
+++ objects/groups.js
@@ -1,6 +1,8 @@
 exports.all =
 	[
-        { firstname: 'Peter', lastname: 'Karlsson', email: 'peter@yttrerymden.com' },
-        { firstname: 'Lorem', lastname: 'Ipsum', email: 'lorem.ipsum@example.com' },
-        { firstname: 'Dolor', lastname: 'Sit Amet', email: 'dolor.sit-amet@example.com' },
+        { firstname: 'Emil', lastname: 'Sundén', email: 'Emil.Sunden@yh.nackademin.se', starSign: 'pisces' },
+        { firstname: 'Elin', lastname: 'Horn', email: 'Elin.Horn@yh.nackademin.se', starSign: 'pisces' },
+        { firstname: 'Tom', lastname: 'Johansson', email: 'Tom.Johansson@yh.nackademin.se', starSign: 'capricorn'},
+        { firstname: 'Bagher', lastname: 'Hosseini', email: 'Bagher.Hosseini@yh.nackademin.se', starSign: 'libra'},
+        { firstname: 'Claude', lastname: 'Loba', email: 'Claude.Loba@yh.nackademin.se', starSign: 'aquarius'},
 ]
