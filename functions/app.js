8e856e4f84572e6981b3cdd5a08b488cf26ba37f
Claude-Marcel Loba <100008570+claudeloba@users.noreply.github.com>
2022-09-29 16:25:45 +0200

    push test
    push test

--- functions/app.js
+++ functions/app.js
@@ -1,11 +1,11 @@
 // TODO add new function as an operator so that the user can type "calculate 2 + 4" from the frontend and get the correct answer
-const square = function(x,y) {
-    return calculateSquare(x,y)
+const square = function (x, y) {
+    return calculateSquare(x, y)
     // return x * y
 }
 
-function calculateSquare(x,y) {
-    return x * y // The return value can be assigned
+function calculateSquare(x, y) {
+    return x * y // The return value can be assigned  
 }
 
 // anonymous functions can be passed as arguments and used in other modules
