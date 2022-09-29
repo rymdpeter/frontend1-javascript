d292915b56730e350d6f07f0b5ea7912c1ca9768
Claude-Marcel Loba <100008570+claudeloba@users.noreply.github.com>
2022-09-29 22:52:33 +0200

    la till emojin som ligger i secrets.json
    emoji

--- www/public/group-4/script.js
+++ www/public/group-4/script.js
@@ -13,7 +13,7 @@ function appendData(data) {
   var mainContainer = document.getElementById("myData");
   for (var i = 0; i < data.length; i++) {
     var div = document.createElement("div");
-    div.innerHTML = 'Name: ' + data[i].firstname + ' ' + data[i].lastname + '.<br>' + 'Discord: ' + data[i].discord + '.<br>' + 'Github: ' + data[i].github + '.<br>' + 'Personality type: ' + data[i].personalityType + '.<br>' + 'Birth year: ' + data[i].birthYear + '.<br>' + 'Zodiac sign: ' + data[i].zodiac.name + '.';
+    div.innerHTML = 'Name: ' + data[i].firstname + ' ' + data[i].lastname + '.<br>' + 'Discord: ' + data[i].discord + '.<br>' + 'Github: ' + data[i].github + '.<br>' + 'Personality type: ' + data[i].personalityType + '.<br>' + 'Birth year: ' + data[i].birthYear + '.<br>' + 'Zodiac sign: ' + data[i].zodiac.name + data[i].zodiac.symbol + '.';
 
     mainContainer.appendChild(div);
   }
