88f3b1abe904fecd07cdfcb3c7f42fbd3ef4d5b2
Claude-Marcel Loba <100008570+claudeloba@users.noreply.github.com>
2022-10-03 13:18:15 +0200

    previous

--- www/public/group-4/script.js
+++ www/public/group-4/script.js
@@ -9,9 +9,6 @@ fetch('./data.json')
       if (i > data.length - 1) {
         i = 0;
       }
-      fetch('./data.json')
-        .then((res) => res.json())
-        .then((data) => {
           document.getElementById("name").textContent = data[i].firstname + " " + data[i].lastname;
           document.getElementById("discord").textContent = data[i].discord;
           document.getElementById("github").textContent = data[i].github;
@@ -20,6 +17,19 @@ fetch('./data.json')
           document.getElementById("birthYear").textContent = data[i].birthYear;
           document.getElementById("zodiac").textContent = data[i].zodiac.name + " " + data[i].zodiac.symbol;
   })
+  let previous = document.getElementById("previous");
+  previous.addEventListener("click", () => {
+    i--;
+    if (i < 0) {
+      i = data.length - 1;
+    }
+        document.getElementById("name").textContent = data[i].firstname + " " + data[i].lastname;
+        document.getElementById("discord").textContent = data[i].discord;
+        document.getElementById("github").textContent = data[i].github;
+        document.getElementById("personalityType").textContent = data[i].personalityType;
+        document.getElementById("favoritmat").textContent = data[i].mat;
+        document.getElementById("birthYear").textContent = data[i].birthYear;
+        document.getElementById("zodiac").textContent = data[i].zodiac.name + " " + data[i].zodiac.symbol;
+      })
   .catch((err) => console.log(err));
-});
 });
